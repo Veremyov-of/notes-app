0e980f28d113e221ca79eee2ab7a07f90dfa53cb
Denis <veremyov.of@gmail.com>
2022-06-12 23:36:19 +0300

    fix local storage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 export const useLocalStorage = (localItem) => {
     
-    const [loc, setState] = useState(localStorage.getItem(localItem) || []);
+    const [loc, setState] = useState(JSON.parse(localStorage.getItem(localItem)) || []);
 
     function setLoc(newItem) {
         localStorage.setItem(localItem, JSON.stringify(newItem));
